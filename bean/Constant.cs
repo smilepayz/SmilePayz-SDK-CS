025c052862938ca18eea25fb6d35db19ebde3893
smilepayz <163798308+smilepayz@users.noreply.github.com>
2024-07-15 09:56:17 +0800

    Update Constant.cs

--- bean/Constant.cs
+++ bean/Constant.cs
@@ -2,9 +2,28 @@
 
 public class Constant
 {
-    public const string privateKeyStr = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3LbmfPhjGXJ+a6NVKyRWEfCpsKfl9UFnMRltNosJv+7qd6OUK3t7Q8fKX8rxuJBeXLLWrOZvjoGlP3ybvvhUSGKH+BLcN1k2eJbcKuThvXvKQvd7pXolY91gkF8V78FX+TKegZupbeji0XUGXCBNyShocYM6Cvailf0Iyv49dcktJmi4drBKfgj5l4HUY9TaDuCZOrhvFZfZZxBK1zkm72k0ZLfqTGmG8O2tsByndCTH2aPLT9odR8/O4qfoTG+vV9HivIKKOI2h0kZfQBOqWD4/ofwu9PNoRRzGgzfRpF3GhnKa3bRVSpCUuBtHfotQfFxbjaDk0s3K5BhQE7HxzAgMBAAECggEARHhaBxUeC595pVzcxVyOp3wGG3JBKL9NIZc277kj9tngcsAoRTzziqS1qmh4WK8zBjYXHg6ln5tJYiqmkjy6AY6llp7KkeiGENRGLEL5vl9+Se4/EXpd2pxyHOOp1N8MNccPbVyqw1DXO0wUhVDme/UI94yUBLjB/kKoSvHhs+qwJ8cz9C8sF3Zs/zF6Te/f+Z+HrIbVj4vlx6DYBs3pWe7J+XYg3XXpbvIBJVTN0lreQAjtopic0F7o1EULnllJmOqy+kiRMuSi5ESrWFOnuCrY0iz/C8LJKlqWa3d+1jVLPvKYucXvddYrwjyu8kYHZAenKWKkLPlQkQxGUFcX8QKBgQDYSCsgg7tDlXTw1P9Z2BhkVo1ugYWA1FcxqTY2sTOMOwCTt3QZAFEqofbNvdjZbk7vu8gh5LyxbWAr/sNqOXH5915a6DZKBGl+qBTH2TZZKmhCUWlmn8T7ySYclfsgIZsUxHaDfY+otiXGtNegSX9US9/AFVSMdiQBCmr5/i48mQKBgQDY0U5+MDl6GnbDH/hCT+YMq/m5W58m7ehJfG6jPtLQkPPvMq2Oj5HUeXx4vDdUxzaRlC9fICwe2KgKiGYpJsRPte6JfO6te7wsO2lDk0oBiw+jewm4CwZ5KeDYyReha8Hc2H1j5hllVx5DIZiage2ZswS5+kCNgzf4QbdAOd886wKBgAo5TyCYWY/WTtLbnr6GgpCrrr/ci40NfJmyYAex1Lf6Sgqxj2FnLG8RfPM42DlfB4g5njpL78eLXhJ2VpJ86LBiSymM9JQHJV2BYIoZ8IHCiW8pHgxl3Q/x8EVFqbtZG1Wd++Q3WUUmZx6/ibnf/47ij08rMvX417bc4TW0GEdxAoGAPX3nUBynQH0e76oyg8QbT766nZphoe3ZcnYK/tuDeMmTlWR/Gq6XQnaOGcPvwWiajmFDqiv6t2jlB8+1gbhP9vd3RqEbJDKypKzY5uRwGc3xyoDLudnOpTB+Z51oyUxBeDwiG+IXk8lIeOufVzrAQ1YlYgWap0fu6MbijSGcsa8CgYEA0OolBxRKdBSxmwMbXDocg5HtHd15UgJdlgL9GFjDZPisUvM7LWK97E5QPhY2IEIcv0EW9jM62bc1uPMhwDuN1dzmLafQIivGsIj5qUy18CoE4KJHsTeKLeHkrd6/nBd4da4A37+ksJ1t/sgZpjl40ShrxTV+OtzzY/kuQ4Uqc9g=";
-    public const string publicKeyStr = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAty25nz4YxlyfmujVSskVhHwqbCn5fVBZzEZbTaLCb/u6nejlCt7e0PHyl/K8biQXlyy1qzmb46BpT98m774VEhih/gS3DdZNniW3Crk4b17ykL3e6V6JWPdYJBfFe/BV/kynoGbqW3o4tF1BlwgTckoaHGDOgr2opX9CMr+PXXJLSZouHawSn4I+ZeB1GPU2g7gmTq4bxWX2WcQStc5Ju9pNGS36kxphvDtrbAcp3Qkx9mjy0/aHUfPzuKn6Exvr1fR4ryCijiNodJGX0ATqlg+P6H8LvTzaEUcxoM30aRdxoZymt20VUqQlLgbR36LUHxcW42g5NLNyuQYUBOx8cwIDAQAB";
+       *Vb18+QwmffJzFAR4Lp4th7HFTkfy/0zJL5AzhK5sVw7obyRlXmQR0gbpAoGAEFHd
+    *Ci0lwA07oxyCCrGA2tmZL3uf+KmTxIM5HOjgxw/b8BWYucnZEbWUQUxXUkbDA7HS
+    *sL71tHcPWBiDfHqVDIjBhwMI6hozmjHE9dAnRb4OMEBj3sJjYR2BkA8mMp3SqmYs
+    *5Hje4LkJ39glNe8fKBoJbJ/w3D9sTd7TMmEOyAECgYAp27u3Txf65wVBV6NY/RdR
+    */vq16/mZlXZ2kH459KX1Zh7hKStmTF7RL1kNQccpmn54PZtfJ1bZlwXzrHT+rvO9
+    *VGnw61ScSmQBfJ9XFB2j0vbm4yRcWnw0I/ycFuEhcGdakUejlxeaeWCxmY6qsY+0
+    *YGC66+5jRKR+T3Vjzi6w2A==
+    **/
+    public const string privateKeyStr =;
 
+      /**
+       *   MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApKQPsmh5d4Y357wIFrqM
+       *   T6ABmzgIlaZKIGdZKECfK/IXRAJB5sxNLmG3sAXUaSWl4t7VJY0mseyJFiVpuZOU
+       *   /rqjFHTdjEQ0RvU8pKXz1TZ1QkypRLhYV8khNTtm/ZY4455Io5Ix/qFsXbtfQSW+
+       *   wist/LglnkGKIU9+l5PYJ+8u1sBk/PL304Gbu3WDPOLmnQQj0tJmjkI3mkRt2Lv2
+       *   Ho3/E8btc2+J/TltfjimC7P4HQclJ3ONfnB22FgCy7Ntu0MdUaSTE/QR8dYptSHS
+       *   yIi1yvSvT06aT14JPgwWriLAG01uWj/r+n3H4S3hAlRv1Jd2ylVaP0kCqjqEqdDH
+       *   ewIDAQAB
+      **/
+    public const string publicKeyStr = "";
+
+ 
 
     public const string baseUrlSanbox = "https://sandbox-gateway.smilepayz.com";
     public const string baseUrl = "https://gateway.smilepayz.com";
